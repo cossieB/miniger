8857b31b9c18340bc63b51b3e6a3409f272e1664
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2025-02-28 20:52:07 +0200

    modified sql schema

--- src-tauri/src/migrations/schema.sql
+++ src-tauri/src/migrations/schema.sql
@@ -7,30 +7,35 @@ CREATE TABLE IF NOT EXISTS actor (
     image TEXT
 );
 
-CREATE TABLE IF NOT EXISTS film (
-    film_id INTEGER NOT NULL PRIMARY KEY,
-    path text NOT NULL UNIQUE,
-    title TEXT NOT NULL,
-    studio_id INTEGER,
-    release_date TEXT
-);
-
 CREATE TABLE IF NOT EXISTS studio (
     studio_id INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     website TEXT
 );
 
+CREATE TABLE IF NOT EXISTS film (
+    film_id INTEGER NOT NULL PRIMARY KEY,
+    path text NOT NULL UNIQUE,
+    title TEXT NOT NULL,
+    studio_id INTEGER,
+    release_date TEXT,
+
+    FOREIGN KEY (studio_id) REFERENCES studio(studio_id) ON DELETE RESTRICT
+);
+
 CREATE TABLE IF NOT EXISTS actor_film (
     actor_id INTEGER NOT NULL,
     film_id INTEGER NOT NULL,
 
-    PRIMARY KEY(actor_id, film_id)
+    PRIMARY KEY(actor_id, film_id),
+    FOREIGN KEY (actor_id) REFERENCES actor(actor_id) ON DELETE CASCADE,
+    FOREIGN KEY (film_id) REFERENCES film(film_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS film_tag (
     film_id INTEGER NOT NULL,
     tag TEXT NOT NULL,
 
-    PRIMARY KEY(film_id, tag)
+    PRIMARY KEY(film_id, tag),
+    FOREIGN KEY (film_id) REFERENCES film(film_id) ON DELETE CASCADE
 );
--- src/api/db.ts
+++ src/api/db.ts
@@ -2,7 +2,7 @@ import Database from "@tauri-apps/plugin-sql";
 
 export async function getDatabase() {
     const connection = await Database.load("sqlite:mngr.db")
-
+    await connection.execute("PRAGMA foreign_keys = ON")
     return {
         connection,
         [Symbol.asyncDispose]: async () => {
